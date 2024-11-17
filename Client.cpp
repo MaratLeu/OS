bd5ef4e884d77560ebfe161dd7f761a23310ef15
MaratLeu <168109373+MaratLeu@users.noreply.github.com>
2024-11-18 02:39:26 +0300

    Add files for 5 lab (on pipes)

---
+++ Client.cpp
@@ -0,0 +1,139 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include <cstring>
+#include <limits>
+
+#ifdef max
+#undef max
+#endif
+
+using namespace std;
+
+struct Employee {
+    int num;
+    char name[20]; 
+    double hours;
+};
+
+struct Message {
+    DWORD type;
+    DWORD client_id;
+    char operation[20]; 
+    int num;
+    Employee data;
+};
+
+void serializeEmployee(const Employee& employee, ofstream& fout) {
+    fout.write((char*)&employee.num, sizeof(employee.num));
+    fout.write(employee.name, sizeof(employee.name)); 
+    fout.write((char*)&employee.hours, sizeof(employee.hours));
+}
+
+void deserializeEmployee(Employee& employee, ifstream& fin) {
+    fin.read((char*)&employee.num, sizeof(employee.num));
+    fin.read(employee.name, sizeof(employee.name)); 
+    fin.read((char*)&employee.hours, sizeof(employee.hours));
+}
+
+int main(int argc, char* argv[]) {
+    DWORD bytesRead;
+    Message message;
+    message.client_id = GetCurrentProcessId();
+
+    wchar_t pipeName[50];
+    wsprintfW(pipeName, L"\\\\.\\pipe\\Employee_pipe");
+
+    HANDLE hNamedPipe = CreateFile(pipeName, GENERIC_READ | GENERIC_WRITE,
+        FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+
+    if (hNamedPipe == INVALID_HANDLE_VALUE) {
+        cerr << "Connection with the named pipe failed: " << GetLastError() << "\n";
+        return 1;
+    }
+
+    string operation;
+    while (true) {
+        cout << "Enter operation (read, write, exit): ";
+        cin >> operation;
+
+        if (operation == "exit") {
+            message.type = 0;
+            strcpy_s(message.operation, "exit");
+            DWORD bytesWritten;
+            if (!WriteFile(hNamedPipe, &message, sizeof(message), &bytesWritten, NULL)) {
+                cerr << "Error writing to pipe: " << GetLastError() << std::endl;
+                break;
+            }
+            break;
+        }
+
+        message.type = 1; 
+        strcpy_s(message.operation, operation.c_str());
+
+        if (operation == "write") {
+            cout << "Enter employee data (num, name, hours): ";
+            cin >> message.data.num >> message.data.name >> message.data.hours;
+
+            if (cin.fail()) {
+                cerr << "Invalid input for employee data.\n";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                continue;
+            }
+
+            if (!WriteFile(hNamedPipe, &message, sizeof(message), NULL, NULL)) {
+                cerr << "Error sending write request: " << GetLastError() << "\n";
+                CloseHandle(hNamedPipe);
+                return 1;
+            }
+
+            Message response;
+            if (!ReadFile(hNamedPipe, &response, sizeof(response), &bytesRead, NULL) || bytesRead == 0) {
+                cerr << "Error receiving confirmation for write operation: " << GetLastError() << "\n";
+            }
+            else if (strcmp(response.operation, "write_ok") != 0) {
+                cerr << "Server returned an error for write operation: " << response.operation << "\n";
+            }
+            else {
+                cout << "Employee written successfully.\n";
+            }
+        }
+        else if (operation == "read") {
+            cout << "Enter employee number to read: ";
+            cin >> message.num;
+
+            if (cin.fail()) {
+                cerr << "Invalid input for employee number.\n";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                continue;
+            }
+
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            strcpy_s(message.operation, "read"); 
+
+            if (!WriteFile(hNamedPipe, &message, sizeof(message), NULL, NULL)) {
+                cerr << "Error sending read request: " << GetLastError() << "\n";
+                CloseHandle(hNamedPipe);
+                return 1;
+            }
+
+            if (!ReadFile(hNamedPipe, &message, sizeof(message), &bytesRead, NULL) || bytesRead == 0) {
+                cerr << "Error reading response from server: " << GetLastError() << "\n";
+                CloseHandle(hNamedPipe);
+                return 1;
+            }
+            else if (strcmp(message.operation, "read_ok") == 0) {
+                cout << "Employee Data:\n";
+                cout << "ID: " << message.data.num << ", Name: " << message.data.name
+                    << ", Hours: " << message.data.hours << "\n";
+            }
+            else {
+                cout << "Incorrect ID, please enter correct ID\n";
+            }
+        }
+    }
+    CloseHandle(hNamedPipe);
+    return 0;
+}
---
+++ Lab_5_Named_pipes.cpp
@@ -0,0 +1,220 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+struct Employee {
+    int num;
+    char name[20];
+    double hours;
+};
+
+struct Message {
+    DWORD type;
+    DWORD client_id;
+    char operation[20]; 
+    int num;
+    Employee data;
+};
+
+string filename;
+
+void serializeEmployee(ofstream& fout, const Employee& employee) {
+    fout.write((char*)&employee.num, sizeof(employee.num));
+    fout.write(employee.name, sizeof(employee.name));
+    fout.write((char*)&employee.hours, sizeof(employee.hours));
+}
+
+void deserializeEmployee(ifstream& fin, Employee& employee) {
+    fin.read((char*)&employee.num, sizeof(employee.num));
+    fin.read(employee.name, sizeof(employee.name));
+    fin.read((char*)&employee.hours, sizeof(employee.hours));
+}
+
+HANDLE* hEvents;
+
+DWORD WINAPI InstanceThread(LPVOID lpvParam) {
+    HANDLE hNamedPipe = HANDLE(lpvParam);
+    Message message;
+    bool clientConnected = true;
+    while (clientConnected) {
+        DWORD bytesRead;
+        BOOL result = ReadFile(hNamedPipe, &message, sizeof(message), &bytesRead, NULL);
+
+        if (strcmp(message.operation, "exit") == 0) {
+            cout << "Client has exited\n";
+            clientConnected = false;
+            DisconnectNamedPipe(hNamedPipe);
+            break;
+        }
+        if (message.type == 0) {
+            result = ReadFile(hNamedPipe, &message.client_id, sizeof(message.client_id), &bytesRead, NULL);
+            result &= ReadFile(hNamedPipe, message.operation, sizeof(message.operation), &bytesRead, NULL);
+            result &= ReadFile(hNamedPipe, &message.num, sizeof(message.num), &bytesRead, NULL);
+            result &= ReadFile(hNamedPipe, &message.data, sizeof(message.data), &bytesRead, NULL);
+            if (!result) {
+                cerr << "Error reading message from client\n";
+                clientConnected = false;
+                break;
+            }
+        }
+
+        else if (message.type == 1) {
+            if (strcmp(message.operation, "write") == 0) {
+                ofstream fout(filename, ios::binary | ios::app);
+                if (fout.is_open()) {
+                    fout.write((char*)&message.data, sizeof(message.data));
+                    fout.close();
+
+                    strcpy_s(message.operation, "write_ok");
+                    WriteFile(hNamedPipe, &message, sizeof(message), NULL, NULL);
+                }
+                else {
+                    cerr << "Error opening file for writing.n";
+                    strcpy_s(message.operation, "write_error");
+                    WriteFile(hNamedPipe, &message, sizeof(message), NULL, NULL);
+                }
+            }
+            else if (strcmp(message.operation, "read") == 0) {
+                ifstream fin(filename, ios::binary);
+                bool found = false;
+
+                if (fin.is_open()) {
+                    Employee emp;
+                    while (fin.read((char*)&emp, sizeof(emp))) {
+                        if (emp.num == message.num) {
+                            message.data = emp;
+                            found = true;
+                            break;
+                        }
+                    }
+                    fin.close();
+
+                    if (found) {
+                        strcpy_s(message.operation, "read_ok");
+                        WriteFile(hNamedPipe, &message, sizeof(message), NULL, NULL);
+                    }
+                    else {
+                        strcpy_s(message.operation, "not_found");
+                        WriteFile(hNamedPipe, &message, sizeof(message), NULL, NULL);
+                    }
+                }
+                else {
+                    cerr << "Error opening file for reading.\n";
+                    strcpy_s(message.operation, "read_error");
+                    WriteFile(hNamedPipe, &message, sizeof(message), NULL, NULL);
+                }
+            }
+        }
+    }
+    DisconnectNamedPipe(hNamedPipe);
+    CloseHandle(hNamedPipe);
+    cout << "Instance thread exiting\n";
+    return 1;
+}
+
+int main() {
+    cout << "Enter name of file: ";
+    cin >> filename;
+
+    ofstream fout(filename, ios::binary);
+    Employee employee;
+    int count;
+    cout << "Enter the amount of records: ";
+    cin >> count;
+
+    cout << "Enter the data about employees (id, name, hours): \n";
+    for (int i = 0; i < count; i++) {
+        cin >> employee.num >> employee.name >> employee.hours;
+        serializeEmployee(fout, employee);
+    }
+    fout.close();
+
+    ifstream fin(filename, ios::binary);
+    cout << "Output contents to console: \n";
+    while (fin.peek() != EOF) {
+        deserializeEmployee(fin, employee);
+        cout << employee.num << "\t" << employee.name << "\t" << employee.hours << "\n";
+    }
+    fin.close();
+
+    int numClients;
+    cout << "Enter num of clients: ";
+    cin >> numClients;
+    DWORD dwThreadId = 0;
+    HANDLE* hProcesses = new HANDLE[numClients];
+    HANDLE hNamedPipe = INVALID_HANDLE_VALUE, hThread = NULL;
+
+    cout << "Waiting for clients...\n";
+
+    for (int i = 0; i < numClients; i++) {
+        wchar_t pipeName[50];
+        wsprintfW(pipeName, L"\\\\.\\pipe\\Employee_pipe");
+        hNamedPipe = CreateNamedPipe(
+            pipeName,
+            PIPE_ACCESS_DUPLEX,
+            PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+            PIPE_UNLIMITED_INSTANCES,
+            sizeof(Message),
+            sizeof(Message),
+            0,
+            NULL
+        );
+
+        if (hNamedPipe == INVALID_HANDLE_VALUE) {
+            cerr << "Creation of named pipe failed: " << GetLastError() << "\n";
+            return 1;
+        }
+
+        wchar_t appName[] = L"C:/Users/Marat Leu/source/repos/Process-server/ARM64/Debug/Client.exe";
+        STARTUPINFO si;
+        PROCESS_INFORMATION pi;
+        ZeroMemory(&si, sizeof(si));
+        si.cb = sizeof(si);
+        ZeroMemory(&pi, sizeof(pi));
+
+        if (!CreateProcess(appName, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
+            cerr << "Error launching client " << i + 1 << ": " << GetLastError() << "\n";
+            return 1;
+        }
+
+        hProcesses[i] = pi.hProcess;
+        if (hProcesses[i] == NULL) {
+            cerr << "Error for creating Process: " << GetLastError() << "\n";
+            return 1;
+        }
+
+        if (!ConnectNamedPipe(hNamedPipe, NULL)) {
+            cerr << "Error connecting to named pipe: " << GetLastError() << "\n";
+            CloseHandle(hNamedPipe);
+            return 1;
+        }
+        else {
+            hThread = CreateThread(NULL, 0, InstanceThread, LPVOID(hNamedPipe), 0, &dwThreadId);
+            if (hThread == NULL) {
+                cerr << "Error creating a thread: " << GetLastError();
+                return 1;
+            }
+            else {
+                CloseHandle(hThread);
+            }
+        }
+    }
+    WaitForMultipleObjects(numClients, hProcesses, TRUE, INFINITE);
+
+    for (int i = 0; i < numClients; i++) {
+        CloseHandle(hProcesses[i]);
+    }
+    delete[] hProcesses;
+    Employee read_employee;
+    ifstream read_bin(filename, ios::binary);
+    cout << "Output contents to console: \n";
+    while (read_bin.peek() != EOF) {
+        deserializeEmployee(read_bin, read_employee);
+        cout << read_employee.num << "\t" << read_employee.name << "\t" << read_employee.hours << "\n";
+    }
+    read_bin.close();
+    return 0;
+}
